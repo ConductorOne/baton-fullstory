d2af6053585f0a96a5a5b4e2ff155b7b66fe2f35
Martin Albert <martin.albert.187@gmail.com>
2024-05-16 14:28:42 +0200

    feat: prepare connector with listing of users

--- cmd/baton-fullstory/main.go
+++ cmd/baton-fullstory/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/conductorone/baton-sdk/pkg/cli"
@@ -11,6 +12,7 @@ import (
 	"github.com/conductorone/baton-sdk/pkg/uhttp"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
+	"golang.org/x/oauth2"
 
 	"github.com/conductorone/baton-fullstory/pkg/connector"
 )
@@ -37,12 +39,33 @@ func main() {
 	}
 }
 
+type CustomBasicAuth struct {
+	Token string
+}
+
+var _ uhttp.AuthCredentials = (*CustomBasicAuth)(nil)
+
+func (c *CustomBasicAuth) GetClient(ctx context.Context, options ...uhttp.Option) (*http.Client, error) {
+	httpClient, err := uhttp.NewClient(ctx, options...)
+	if err != nil {
+		return nil, fmt.Errorf("creating HTTP client failed: %w", err)
+	}
+
+	ctx = context.WithValue(ctx, oauth2.HTTPClient, httpClient)
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: c.Token, TokenType: "basic"},
+	)
+	httpClient = oauth2.NewClient(ctx, ts)
+
+	return httpClient, nil
+}
+
 func getConnector(ctx context.Context, cfg *config) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
 
 	var auth uhttp.AuthCredentials = &uhttp.NoAuth{}
 	if cfg.Token != "" {
-		auth = uhttp.NewBasicAuth(cfg.Token, "")
+		auth = &CustomBasicAuth{Token: cfg.Token}
 	}
 
 	cb, err := connector.New(ctx, auth)
