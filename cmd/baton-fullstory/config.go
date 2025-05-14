4e1a36a196b86d1a17c576dff2e764b146190d51
MB Burch <mb.burch@conductorone.com>
2025-05-14 09:12:55 -0600

    Mbburch/bb 701 lambda prep (#28)

--- cmd/baton-fullstory/config.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/conductorone/baton-sdk/pkg/field"
-)
-
-var (
-	apikey = field.StringField("api-key", field.WithRequired(true), field.WithDescription("FullStory API Key to authenticate with"))
-)
--- cmd/baton-fullstory/main.go
+++ cmd/baton-fullstory/main.go
@@ -5,11 +5,10 @@ import (
 	"fmt"
 	"os"
 
+	cfg "github.com/conductorone/baton-fullstory/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
-	"github.com/conductorone/baton-sdk/pkg/field"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
-	"github.com/spf13/viper"
 	"go.uber.org/zap"
 
 	"github.com/conductorone/baton-fullstory/pkg/connector"
@@ -21,7 +20,7 @@ var version = "dev"
 func main() {
 	ctx := context.Background()
 
-	_, cmd, err := configschema.DefineConfiguration(ctx, "baton-fullstory", getConnector, field.Configuration{Fields: []field.SchemaField{apikey}})
+	_, cmd, err := configschema.DefineConfiguration(ctx, "baton-fullstory", getConnector, cfg.Config)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
@@ -36,10 +35,10 @@ func main() {
 	}
 }
 
-func getConnector(ctx context.Context, v *viper.Viper) (types.ConnectorServer, error) {
+func getConnector(ctx context.Context, fsc *cfg.Fullstory) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
 
-	apiKey := v.GetString("api-key")
+	apiKey := fsc.ApiKey
 
 	cb, err := connector.New(ctx, apiKey)
 	if err != nil {
