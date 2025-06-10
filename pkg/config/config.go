cca81baf5a5739f89e2aa75ffb33e317503c3f27
MB Burch <mb.burch@conductorone.com>
2025-06-10 13:19:15 -0600

    Add field and config options (#38)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -5,10 +5,21 @@ import (
 )
 
 var (
-	APIKeyField = field.StringField("api-key", field.WithRequired(true), field.WithDescription("FullStory API Key to authenticate with"))
+	APIKeyField = field.StringField(
+		"api-key",
+		field.WithDisplayName("API key"),
+		field.WithDescription("FullStory API Key to authenticate with"),
+		field.WithRequired(true),
+		field.WithIsSecret(true),
+	)
 )
 
 //go:generate go run ./gen
-var Config = field.NewConfiguration([]field.SchemaField{
-	APIKeyField,
-})
+var Config = field.NewConfiguration(
+	[]field.SchemaField{
+		APIKeyField,
+	},
+	field.WithConnectorDisplayName("Fullstory"),
+	field.WithHelpUrl("/docs/baton/fullstory"),
+	field.WithIconUrl("/static/app-icons/fullstory.svg"),
+)
