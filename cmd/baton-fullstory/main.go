f47791425756baa4bfd8ddca5d560415f94aaf88
Steve Gontzes <steve.gontzes@conductorone.com>
2026-01-28 16:47:11 -0500

    Upgrade Go to 1.25.2, baton-sdk to v0.7.4, update CI and golangci-lint v2 (#69)

--- cmd/baton-fullstory/main.go
+++ cmd/baton-fullstory/main.go
@@ -7,6 +7,7 @@ import (
 
 	cfg "github.com/conductorone/baton-fullstory/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
@@ -20,7 +21,13 @@ var version = "dev"
 func main() {
 	ctx := context.Background()
 
-	_, cmd, err := configschema.DefineConfiguration(ctx, "baton-fullstory", getConnector, cfg.Config)
+	_, cmd, err := configschema.DefineConfiguration(
+		ctx,
+		"baton-fullstory",
+		getConnector,
+		cfg.Config,
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.FullStory{}),
+	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
