04c9680bfb818b74053f934e3cf7e4e321beacbb
Robert Chiniquy <rchiniquy@yahoo.com>
2026-02-18 16:04:01 -0800

    Add ExportTargetCLIOnly to base-url field
    Ref: https://github.com/ConductorOne/baton-prismhr/pull/13#discussion_r2790908601

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -16,6 +16,7 @@ var (
 		"base-url",
 		field.WithDescription("Override the FullStory API URL (for testing)"),
 		field.WithHidden(true),
+		field.WithExportTarget(field.ExportTargetCLIOnly),
 	)
 )
 
